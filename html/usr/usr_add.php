aaf61775dbbfe1431bbf3be5450b5c69b5cf07c9
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2020-07-15 20:12:07 -0300

    v 0.3.0

--- html/usr/usr_add.php
+++ html/usr/usr_add.php
@@ -8,12 +8,14 @@
 	<?php
 		if (isset($_POST['usr_nick']))
 		{
+			//$f = "sudo samba-tool user create {$_POST['usr_nick']} {$_POST['usr_pass1']} --home-directory {$_POST['usr_dir']} --given-name {$_POST['usr_nome']} --surname {$_POST['usr_snome']} --mail-address {$_POST['usr_email']} --telephone-number {$_POST['usr_tel']}";
+			
 			$f = "sudo samba-tool user create {$_POST['usr_nick']} {$_POST['usr_pass1']}";
 
-			/*if (document.getElementById('usr_dir').value != '')
+			if (document.getElementById('usr_dir').value != '')
 			{
-				$f=$f." --home-directory {$_POST['usr_dir']};"
-			}*/
+				$f=$f." --home-directory {$_POST['usr_dir']}";
+			}
 			if (document.getElementById('usr_nome').value != '')
 			{
 				$f=$f." --given-name {$_POST['usr_nome']}";
