f2b6b4c84ea91f6e185db2a91e820c1247afe81a
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2020-07-18 15:24:11 -0300

    v 0.3.3

--- html/grp/grp_rem.php
+++ html/grp/grp_rem.php
@@ -9,24 +9,43 @@
 		<header>
 			<?php include("../componentes/navbar.php"); ?>
 		</header>
-		<nav id="nav1"></nav>
+		<nav id="nav1">
+			Grupos:
+			<?php
+				$grplist = `sudo samba-tool group list`;
+				echo "<pre>$grplist</pre>";
+			?>
+		</nav>
 		<section>
 			<h1>
 				<b>Remover Grupo:</b>
 			</h1>
-			<form>
+			<form name="formGrp" method="POST">
 				<p>
 					<b>Informe o nome do grupo:</b>
 					<br>
-					<input type="text" name="grp_nome" placeholder="Ex.: escritorio">
+					<input type="text" name="grp_nick" id="grp_nick" placeholder="Ex.: escritorio">
 				</p>
 				<p>
 					<br>
 					<br>
-					<input type="button" onclick="" value="Confirmar">
+					<input type="button" onclick="DelGrpFunction();" value="Confirmar">
 				</p>
 			</form>
 		</section>
+
+		<br>
+		<br>
+		<br>
+		<?php
+			if (isset($_POST['grp_nick']))
+			{
+				$f = "sudo samba-tool group remove {$_POST['grp_nick']}";
+				
+				$comando = shell_exec($f);
+				echo "<pre>$comando</pre>";
+			}
+		?>
 		<br>
 		<br>
 		<br>
@@ -34,4 +53,17 @@
 			<?php include("../componentes/footerbar.php"); ?>
 		</footer>
 	</body>
+	<script type="text/javascript">
+		function DelGrpFunction()
+		{
+			if (document.getElementById('grp_nick').value == '')
+			{
+				alert("Informe o nome do grupo!");
+			}
+			else
+			{ 
+				document.formGrp.submit();
+			}
+		}
+	</script>
 </html>
