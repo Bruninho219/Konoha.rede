d8e764454ff6a0044bbed702b8b2109de52e6e34
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2020-07-18 14:52:24 -0300

    v 0.3.3

--- html/grp/grp_add.php
+++ html/grp/grp_add.php
@@ -14,11 +14,11 @@
 			<h1>
 				<b>Criar grupo:</b>
 			</h1>
-			<form>
+			<form name="formGrp" method="POST">
 				<p>
 					<b>Informe o nome de grupo:</b>
 					<br>
-					<input type="text" name="grp_nome" id="grp_nick" placeholder="Ex.: escritorio1">
+					<input type="text" name="grp_nome" id="grp_nick" placeholder="Ex.: escritorio_1">
 				</p>
 				<p>
 					<b>Email:</b>
@@ -26,27 +26,48 @@
 					<input type="email" name="grp_email" id="grp_email" placeholder="Ex.: escritorio1@x.com">
 				</p>
 				<p>
-					<b>Informe o nome do grupo :</b>
-					<br>
-					<input type="text" name="grp_nick" placeholder="Ex.: Escritorio 1">
-				</p>
-				<p>
-					<b>Diretório:</b>
-					<br>
-					&nbsp;/var/
-					<input type="text" name="grp_dir" placeholder="">
-				</p>
-				<p>
 					<br>
 					<br>
-					<input type="button" onclick="" value="Criar">
-					&nbsp;&nbsp;&nbsp;
-					<input type="button" onclick="" value="Cancelar">
+					<input type="button" onclick="AddGrpFunction();" value="Criar">
 				</p>
 			</form>
 		</section>
+		<br>
+		<br>
+		<br>
+		<?php
+			if (isset($_POST['grp_nick']))
+			{
+				$f = "sudo samba-tool group add {$_POST['grp_nick']}";
+				
+				if ($_POST['grp_email'] != '')
+				{
+					$f=$f." --mail-address {$_POST['grp_email']}";
+				}
+
+				$comando = shell_exec($f);
+				echo "<pre>$comando</pre>";
+			}
+		?>
+		<br>
+		<br>
+		<br>
 		<footer>
 			<?php include("../componentes/footerbar.php"); ?>
 		</footer>
 	</body>
+
+	<script type="text/javascript">
+		function AddGrpFunction()
+		{
+			if (document.getElementById('grp_nick').value == '')
+			{
+				alert("Informe o nome do grupo!");
+			}
+			else
+			{ 
+				document.formUser.submit();
+			}
+		}
+	</script>
 </html>
