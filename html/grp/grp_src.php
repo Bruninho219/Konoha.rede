bf6390fa8c76e547294cd8cabbc3f96161e83fb5
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2020-07-18 15:47:14 -0300

    v 0.3.3

--- html/grp/grp_src.php
+++ html/grp/grp_src.php
@@ -41,13 +41,7 @@
 		<?php
 			if (isset($_POST['grp_nick']))
 			{
-				$f = "sudo samba-tool group show {$_POST['grp_nick']} ";
-				$comando = shell_exec($f);
-				echo "<pre>$comando</pre>";
-
-				echo "Membros:";
-				echo "";
-				$f = "sudo samba-tool group listmembers {$_POST['grp_nick']} ";
+				$f = "sudo samba-tool group show {$_POST['grp_nick']} ; echo Membros ; sudo samba-tool group listmembers {$_POST['grp_nick']}";
 				$comando = shell_exec($f);
 				echo "<pre>$comando</pre>";
 			}
