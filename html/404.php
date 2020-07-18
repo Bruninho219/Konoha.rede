3cc01164024b4875e7ca630fffbd72036189673f
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2020-07-18 12:55:11 -0300

    v 0.3.3

--- html/404.php
+++ html/404.php
@@ -9,10 +9,15 @@
 		<header>
 			<?php include("../componentes/navbar.php"); ?>
 		</header>
-		<nav id="nav1" ></nav>
 		<section>
 			<p>
-				Chama o Hokage!!!
+				<h1><b>Error 404!</b></h1>
+			</p>
+			<p>
+				<h2>Alguém deveria corrigir isso né!?</h2>
+			</p>
+			<p>
+				<img src="../componentes/404.jpg" width=100%>
 			</p>
 		</section>
 		<footer>
