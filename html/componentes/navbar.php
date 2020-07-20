d64574e22c4a3ea9b08bc195d11859049e268981
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2020-07-20 18:00:07 -0300

    v 0.3.4

--- html/componentes/navbar.php
+++ html/componentes/navbar.php
@@ -40,7 +40,6 @@
 						<!--GRUPO-->
 						<li><a href="../grp/grp_addm.php">Adicionar membro</a></li>
 						<li><a href="../grp/grp_add.php">Criar</a></li>
-						<li><a href="../grp/grp_edt.php">Editar</a></li>
 						<li><a href="../grp/grp_src.php">Pesquisar</a></li>
 						<li><a href="../grp/grp_rem.php">Remover</a></li>
 						<li><a href="../grp/grp_remm.php">Remover membro</a></li>
