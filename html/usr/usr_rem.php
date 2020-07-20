e88158e19a9835ea61d1ec009a8c2a823095f02e
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2020-07-20 16:22:22 -0300

    v 0.3.4

--- html/usr/usr_rem.php
+++ html/usr/usr_rem.php
@@ -57,11 +57,7 @@
 		<?php
 			if (isset($_POST['usr_nick']))
 			{
-				//$fn = filter_input(INPUT_POST, 'alt_func');
-				$fn = $_POST['alt_func'];
-				$f = "sudo samba-tool user "
-				$f = $f. $fn;
-				$f = $f. "{$_POST['usr_nick']}";
+				$f = "sudo samba-tool user {$_POST['alt_func']} {$_POST['usr_nick']}";
 				$comando = shell_exec($f);
 				echo "<pre>$comando</pre>";
 			}
