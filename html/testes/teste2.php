426e86af72553b735966ce9e990580851d5f0b31
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2020-09-18 12:24:12 -0300

    v 0.4.1

--- html/testes/teste2.php
+++ html/testes/teste2.php
@@ -29,8 +29,36 @@
 				print "<p>Versão mais recente:<br>";
 				print $var2[0];
 				print "</p>"
+			?>
+			<?php
 
+				$site_url = "https://github.com/Bruninho219/Konoha.rede/blob/master/html/componentes/versao"
+				$ch = curl_init();
+				$timeout = 0;
+				curl_setopt ($ch, CURLOPT_URL, $site_url);
+				curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+				ob_start();
+				curl_exec($ch);
+				curl_close($ch);
+				$file_contents = ob_get_contents();
+				ob_end_clean();
 				
+				$var1 = explode('<td id=\"LC1" class="blob-code blob-code-inner js-file-line">', $file_contents;);
+				$var2 = explode("</td>",$var1[1]);
+				print "<p>Versão mais recente2:<br>";
+				print $var2[0];
+				print "</p>"
+			}
+
+			<?php
+				$url = "https://github.com/Bruninho219/Konoha.rede/blob/master/html/componentes/versao";
+				$dado = file_get_contents($url);
+				
+				$var1 = explode('<td id=\"LC1" class="blob-code blob-code-inner js-file-line">', $dado);
+				$var2 = explode("</td>",$var1[1]);
+				print "<p>Versão mais recente3:<br>";
+				print $var2[0];
+				print "</p>"
 			?>
 		</section>
 		<footer>
