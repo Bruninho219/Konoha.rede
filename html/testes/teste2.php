03d61013bd47db84262a451b0efba076e27d6d36
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2020-09-18 15:06:08 -0300

    v 0.4.1

--- html/testes/teste2.php
+++ html/testes/teste2.php
@@ -10,11 +10,40 @@
 			<?php include("../componentes/navbar.php"); ?>
 		</header>
 		<nav id="nav1">
-		</form>
+			<?php
+				if (!function_exists('curl_init'))
+				{
+					print "cURL não está instalado!";
+				}
+				else
+				{
+					print "OK!";
+				}
+			?>
 		</nav>
 		<section>
 			<!--***************-->
-			
+			<?php
+				if (!function_exists('curl_init'))
+				{
+					die('cURL não está instalado!');
+				}
+				else
+				{
+					$url = "https://github.com/Bruninho219/Konoha.rede/blob/master/html/componentes/versao";
+					$ch = curl_init();
+					curl_setopt($ch, CURLOPT_URL, $url);
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+					$output = curl_exec($ch);
+					curl_close($ch);
+
+					$start = strpos($output, '<td id="LC1" class="blob-code blob-code-inner js-file-line">');
+					$end = strpos($output, '</td>', $start);
+					$length = $end-$start;
+					$output = substr($output, $start, $length)
+					print $output;
+				}
+			?>
 			<!--***************-->
 		</section>
 		<footer>
