a060341d28eddce0492abea9a5f51031ac04a1f3
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2020-09-18 15:00:19 -0300

    v 0.4.1

--- html/testes/teste2.php
+++ html/testes/teste2.php
@@ -14,28 +14,7 @@
 		</nav>
 		<section>
 			<!--***************-->
-			<?php
-				function curl_download()
-				{
-					if (!function_exists('curl_init'))
-					{
-						die('cURL não está instalado!');
-					}
-
-					$Url = "https://github.com/Bruninho219/Konoha.rede/blob/master/html/componentes/versao";
-					$ch = curl_init();
-					curl_setopt($ch, CURLOPT_URL, $Url);
-					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-					$output = curl_exec($ch);
-					curl_close($ch);
-
-					$start = strpos($output, '<td id="LC1" class="blob-code blob-code-inner js-file-line">');
-					$end = strpos($output, '</td>', $start);
-					$length = $end-$start;
-					return $output = substr($output, $start, $length)
-				}
-				print curl_download('http://www.gutenberg.org/browse/scores/top');
-			?>
+			
 			<!--***************-->
 		</section>
 		<footer>
