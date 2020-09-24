885be676a45bb2d13b15f67106dba76c8613bf1c
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2020-09-24 09:12:54 -0300

    v 0.5.0

--- html/componentes/navbar2.php
+++ html/componentes/navbar2.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="margin:16px;">
 	<a class="navbar-brand" href="#">
-		<img src="img/icon.png" height="30" width="30" />
+		<img src="./../img/konoha.png" height="30" width="30" />
 		<sub style="font-size: 12px;">
 		Konoha</sup>
 	</a>
@@ -18,40 +18,40 @@
 					Usuário
 				</a>
 				<div class="dropdown-menu">
-					<a class="dropdown-item" href="./usr/usr_add3.php">Adicionar</a>
-					<a class="dropdown-item" href="./usr/usr_edt.php">Editar</a>
-					<a class="dropdown-item" href="./usr/usr_src2.php">Pesquisar</a>
-					<a class="dropdown-item" href="./usr/usr_rec.php">Recuperar senha</a>
-					<a class="dropdown-item" href="./usr/usr_rem.php">Remover</a>
-					<a class="dropdown-item" href="./usr/usr_sec.php">Segurança</a>
+					<a class="dropdown-item" href="./../usr/usr_add3.php">Adicionar</a>
+					<a class="dropdown-item" href="./../usr/usr_edt.php">Editar</a>
+					<a class="dropdown-item" href="./../usr/usr_src2.php">Pesquisar</a>
+					<a class="dropdown-item" href="./../usr/usr_rec.php">Recuperar senha</a>
+					<a class="dropdown-item" href="./../usr/usr_rem.php">Remover</a>
+					<a class="dropdown-item" href="./../usr/usr_sec.php">Segurança</a>
 				</div>
 			</li>
 			<li class="nav-item">
 				<a class="nav-link">Grupo</a>
 				<div class="dropdown-menu">
-					<a class="dropdown-item" href="./grp/grp_addm.php">Adicionar membro</a>
-					<a class="dropdown-item" href="./grp/grp_remm.php">Remover membro</a>
-					<a class="dropdown-item" href="./grp/grp_add.php">Criar</a>
-					<a class="dropdown-item" href="./grp/grp_src2.php">Pesquisar</a>
-					<a class="dropdown-item" href="./grp/grp_rem.php">Remover</a>
+					<a class="dropdown-item" href="./../grp/grp_addm.php">Adicionar membro</a>
+					<a class="dropdown-item" href="./../grp/grp_remm.php">Remover membro</a>
+					<a class="dropdown-item" href="./../grp/grp_add.php">Criar</a>
+					<a class="dropdown-item" href="./../grp/grp_src2.php">Pesquisar</a>
+					<a class="dropdown-item" href="./../grp/grp_rem.php">Remover</a>
 				</div>
 			</li>
 			<li class="nav-item">
 				<a class="nav-link">Compartilhamento</a>
 				<div class="dropdown-menu">
-					<a class="dropdown-item" href="./comp/comp_addImp.php">Adicionar impressora</a>
-					<a class="dropdown-item" href="./comp/comp_allImp.php">Comp.geral de impressora</a>
-					<a class="dropdown-item" href="./comp/comp_edtImp.php">Editar impressora</a>
-					<a class="dropdown-item" href="./comp/comp_delImp.php">Remover impressora</a>
-					<a class="dropdown-item" href="./comp/comp_addPasta.php">Adicionar pasta</a>
-					<a class="dropdown-item" href="./comp/comp_edtPasta.php">Editar pasta</a>
-					<a class="dropdown-item" href="./comp/comp_delPasta.php">Remover pasta</a>
+					<a class="dropdown-item" href="./../comp/comp_addImp.php">Adicionar impressora</a>
+					<a class="dropdown-item" href="./../comp/comp_allImp.php">Comp.geral de impressora</a>
+					<a class="dropdown-item" href="./../comp/comp_edtImp.php">Editar impressora</a>
+					<a class="dropdown-item" href="./../comp/comp_delImp.php">Remover impressora</a>
+					<a class="dropdown-item" href="./../comp/comp_addPasta.php">Adicionar pasta</a>
+					<a class="dropdown-item" href="./../comp/comp_edtPasta.php">Editar pasta</a>
+					<a class="dropdown-item" href="./../comp/comp_delPasta.php">Remover pasta</a>
 				</div>
 			</li>
 			<li class="nav-item">
 				<a class="nav-link">Segurança</a>
 				<div class="dropdown-menu">
-					<a class="dropdown-item" href="./sec/sec_pass.php">Senha</a>
+					<a class="dropdown-item" href="./../sec/sec_pass.php">Senha</a>
 					<a class="dropdown-item" href="#">.</a>
 					<a class="dropdown-item" href="#">.</a>
 					<a class="dropdown-item" href="#">.</a>
