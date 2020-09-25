e6c4650b82f495beee69dc93043dd26b437416d5
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2020-09-25 12:26:16 -0300

    v 0.5.0

--- html/sec/sec_validacao.php
+++ html/sec/sec_validacao.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<title>Konoha</title>
-		<link rel="stylesheet" type="text/css" href="./../css/usr.css">
+		<link rel="stylesheet" type="text/css" href="./../css/index.css">
 		<link rel="shortcut icon" href="./../img/icon.png">
 	</head>
 	
