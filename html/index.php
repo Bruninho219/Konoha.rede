221d5e03f597c2e0b50ad38a93eedbfc46cc7a6b
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2020-11-12 16:25:57 -0300

    v 0.5.4

--- html/index.php
+++ html/index.php
@@ -35,7 +35,7 @@
 					$var1 = explode('<td id="LC1" class="blob-code blob-code-inner js-file-line">', $output);
 					$var2 = explode('</td>',$var1[1]);
 					
-					print "<p><b>Versão mais atual: ".$var2[0]."<br>";
+					print "<p><b>Versão mais recente: ".$var2[0]."<br>";
 				}
 			?>
 			<!--
