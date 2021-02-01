6c3a82f9909cf8c7d45df7474faabb4960c811b2
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2021-02-01 10:23:07 -0300

    v 0.5.5

--- html/usr/usr_add3.php
+++ html/usr/usr_add3.php
@@ -8,7 +8,10 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-		<link rel="shortcut icon" href="../img/icon.png" href="./../index.php">
+		<a class="navbar-brand" href="../index.php">
+			<img src="../img/konoha.png" width="25" height="25"/>
+			<link rel="shortcut icon" href="../img/icon.png" href="./../index.php">
+		</a>
 		<style>
 			*
 			{
