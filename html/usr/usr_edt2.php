a74258f0069e3faac41e5bb24f67c248419df11f
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2021-02-05 20:01:19 -0300

    v 0.6.1

--- html/usr/usr_edt2.php
+++ html/usr/usr_edt2.php
@@ -59,7 +59,7 @@
 	<?php
 		if (isset($_POST['usr_nick']))
 		{
-			$f = "sudo samba-tool user edit {$_POST['usr_nick']}";
+			$f = "sudo samba-tool user edit {$_POST['usr_nick']} --editor=nano";
 			$comando = shell_exec($f);
 			//echo "Verificar um método secundário de editar usuário!";
 			//echo "<pre>$comando</pre>";
