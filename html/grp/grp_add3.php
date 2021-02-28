6649af03b3b58e099356370f5079435dd41dd5d6
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2021-02-28 18:59:21 -0300

    v 0.7.0

--- html/grp/grp_add3.php
+++ html/grp/grp_add3.php
@@ -19,56 +19,48 @@
 		<?php
 			include "./../componentes/navbar2.php";
 		?>
+
 		<div class="container">
-			<h2>Adição de grupos:</h2>
+			<h2>Criação de grupo:</h2>
 			<form class="form" role="form" autocomplete="off" method="POST">
 
 				<div class="container py-3">
 					<div class="row">
 						<div class="mx-auto col-sm-12">
+							<!-- form user info -->
 							<div class="card">
-								<div class="card-header">
-									<h6 class="mb-0">Informações obrigatórias</h4>
-								</div>
-								<div class="card-body">
-									<div class="form-group row">
-										<label class="col-lg-3 col-form-label form-control-label">Grupo*</label>
-										<div class="col-lg-9">
-											<input class="form-control" type="text" name="grp_nick" value="">
-										</div>
+									<div class="card-header">
+										<h6 class="mb-0">Informações obrigatórias</h4>
 									</div>
-									
-									<hr>
-									
-									<div class="form-group row">
-										<div class="col-lg-12 text-right">
-											<div class="col-lg-1 text-center">
+									<div class="card-body">
+										<div class="form-group row">
+											<label class="col-lg-3 col-form-label form-control-label">Grupo*</label>
+											<div class="col-lg-9">
+												<input class="form-control" type="text" name="grp_nick" value="">
+											</div>
+										</div>
+
+										<hr>
+
+										<div class="form-group row">
+											<label class="col-lg-3 col-form-label form-control-label">E-mail:</label>
+											<div class="col-lg-9">
 												<input class="form-control" type="email" name="grp_email" value="">
 											</div>
 										</div>
 									</div>
-									<!--
-									<hr>
+								</div>
+				
 
-									<div class="form-group row">
-										<div class="col-lg-12 text-right">
-											<div class="col-lg-1 text-center">
-												<input class="form-control" type="email" name="grp_email" value="">
-											</div>
-										</div>
+								<div class="form-group row">
+									<div class="col-lg-12 text-right">
+										<input type="reset" class="btn btn-secondary" value="Cancelar">
+										<input type="submit" name="submit" class="btn btn-primary" value="Criar"
+											onclick="AddGrpFunction();">
 									</div>
-
-									-->
 								</div>
 							</div>
-
-							<div class="form-group row">
-								<div class="col-lg-12 text-right">
-									<input type="reset" class="btn btn-secondary" value="Cancelar">
-									<input type="submit" name="submit" class="btn btn-primary" value="Criar"
-										onclick="AddGrpFunction();">
-								</div>
-							</div>
+							<!-- /form user info -->
 						</div>
 					</div>
 				</div>
@@ -76,24 +68,24 @@
 		</div>
 	</body>
 	<?php
-		if (isset($_POST['grp_nick']))
-		{
-			$f = "sudo samba-tool group add {$_POST['grp_nick']}";
-			
-			if ($_POST['grp_email'] != '')
+			if (isset($_POST['grp_nick']))
 			{
-				$f=$f." --mail-address {$_POST['grp_email']}";
+				$f = "sudo samba-tool group add {$_POST['grp_nick']}";
+				
+				if ($_POST['grp_email'] != '')
+				{
+					$f=$f." --mail-address {$_POST['grp_email']}";
+				}
+				/*
+				if ($_POST['grp_desc'] != '')
+				{
+					$f=$f." --description {$_POST['grp_desc']}"."/0";
+				}
+				*/
+				$comando = shell_exec($f);
+				echo "<pre>$comando</pre>";
 			}
-			/*
-			if ($_POST['grp_desc'] != '')
-			{
-				$f=$f." --description {$_POST['grp_desc']}"."/0";
-			}
-			*/
-			$comando = shell_exec($f);
-			//echo "<pre>$comando</pre>";
-		}
-	?>
+		?>
 	<br>
 	<br>
 	<br>
