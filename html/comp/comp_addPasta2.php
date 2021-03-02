b40de62de863ace32bb1d1b02aea469adb8bf5b1
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2021-03-01 21:10:57 -0300

    v 0.7.1

--- html/comp/comp_addPasta2.php
+++ html/comp/comp_addPasta2.php
@@ -49,9 +49,11 @@
 										<br>
 										
 										<div class="col-lg-3">
-											/Konoha/samba/
-											<input class="form-control" type="text" name="comp_dir" value="" placeholder="Ex.: PastaLocal">
-											/Pasta_a_ser_criada
+											<p>
+												/Konoha/samba/
+												<input class="form-control" type="text" name="comp_dir" value="" placeholder="Ex.: PastaLocal">
+												/Pasta_a_ser_criada
+											</p>
 										</div>
 									</div>
 									<div class="form-group row">
